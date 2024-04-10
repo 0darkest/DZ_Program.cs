ac09208b1043beeb1a5440e687c49ea6d4da9023
0darkest <darkest0@mail.ru>
2024-04-10 19:33:12 +0300

    Домашняя работа

---
+++ DZ_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+using System;
+
+class Program
+{
+   static void Main(string[] args)
+   {
+       int M = 4;
+       int N = 8;
+       PrintNumbersInRange(M, N);
+   }
+   static void PrintNumbersInRange(int start, int end)
+   {
+       if (start <= end)
+       {
+           Console.Write(start);
+           if (start < end)
+               Console.Write(", ");
+           PrintNumbersInRange(start + 1, end);
+       }
+   }
+}
---
+++ DZ_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        int[] myArray = { 1, 2, 5, 0, 10, 34 }; 
+ 
+        Console.WriteLine("Элементы массива, начиная с конца:"); 
+        PrintArrayReverse(myArray, myArray.Length - 1); 
+    } 
+ 
+    static void PrintArrayReverse(int[] arr, int index) 
+    { 
+        if (index >= 0) 
+        { 
+            Console.Write(arr[index] + " "); 
+            PrintArrayReverse(arr, index - 1); 
+        } 
+    } 
+}
